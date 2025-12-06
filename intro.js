553eb75c4000eab570e76a2efcc55b04e2c023c2
Muslimah Sulaimon <sulaimonmuslimah1@gmail.com>
2025-12-06 22:58:30 +0100

    Initial commit: Hello world project

---
+++ intro.js
@@ -0,0 +1,5 @@
+const name = "Muslimah Sulaimon"
+const age = 18
+const fav_programming_language = "Python"
+
+console.log("My name is Muslimah Sulaimon, i'm 18 years old, and my favourite programming language is Python")
