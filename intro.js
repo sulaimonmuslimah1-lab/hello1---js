e4e37fc6efdfe442c580e5f1de12dcd2cface8a9
sulaimonmuslimah1-lab <sulaimonmuslimah1@gmail.com>
2025-12-06 23:16:05 +0100

    Remove variable declarations from intro.js

--- intro.js
+++ intro.js
@@ -1,5 +1,1 @@
-const name = "Muslimah Sulaimon"
-const age = 18
-const fav_programming_language = "Python"
-
 console.log("My name is Muslimah Sulaimon, i'm 18 years old, and my favourite programming language is Python")
